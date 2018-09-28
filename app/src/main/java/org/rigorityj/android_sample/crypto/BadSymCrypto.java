3e1c44794f2734dbb054a629fe11ec22ca9b077d
RigorityJ <rigorityj@gmail.com>
2018-09-27 21:37:45 -0400

    Add sources

---
+++ app/src/main/java/org/rigorityj/android_sample/crypto/BadSymCrypto.java
@@ -0,0 +1,36 @@
+package org.rigorityj.android_sample.crypto;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+/**
+ * Created by krishnokoli on 7/2/17.
+ */
+public class BadSymCrypto {
+
+
+    private String algo = "AES/CBC/PKCS5PADDING";
+
+    public BadSymCrypto(String algo) {
+        this.algo = algo;
+    }
+
+    public String decrypt(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance(algo);
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(encrypted.getBytes());
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ app/src/main/java/org/rigorityj/android_sample/crypto/VeryBadMain.java
@@ -0,0 +1,31 @@
+package org.rigorityj.android_sample.crypto;
+
+import java.security.KeyPair;
+
+public class VeryBadMain {
+
+    public static void main(String[] args) throws Exception {
+
+        BadAssymCrypto badCrypto = new BadAssymCrypto();
+        KeyPair kp = badCrypto.generateKeyPairExplicit(512);
+
+        System.out.println(kp.getPrivate().getEncoded());
+
+        BadDigestAndRandom badDigestAndRandom = new BadDigestAndRandom();
+
+        System.out.println(new String(badDigestAndRandom.getDigest("MD5", "Hello World!")));
+        System.out.println(new String(badDigestAndRandom.getDigest("SHA1", "Hello World!")));
+        System.out.println(new String(badDigestAndRandom.getDigest("SHA-512", "Hello World!")));
+
+        String key = "my+secret+key+lol";
+        String initVector = "RandomInitVector";
+        BadSymCrypto crypto = new BadSymCrypto("AES/CBC/PKCS5PADDING");
+
+        String decrypted = crypto.decrypt(key, initVector, "154asf15as4d5as4dasdfasf1sa5d");
+        System.out.println(decrypted);
+
+        String encrypted = PasswordUtils.encryptPassword("mypass,PBEWITHHMACSHA1ANDAES_128,my-sensitive-key,f77aLYLo,2000");
+        System.out.println(encrypted);
+        System.out.println(PasswordUtils.encryptPassword(encrypted));
+    }
+}
